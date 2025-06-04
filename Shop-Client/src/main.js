fafb9c84dbdb2e3244a14e9e611f40d05a6a402c
thyonix <dongf7889@gmail.com>
2025-06-04 22:03:52 +0800

    [Shop-Client] - Import RemixIcon

--- Shop-Client/src/main.js
+++ Shop-Client/src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
+// Global Style
 import './assets/style/globalStyle.css'
+// Import ElementPlus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+// Import RemixIcon
+import 'remixicon/fonts/remixicon.css'
+// Import VueRouter
 import router from './router'
 import App from './App.vue'
 
