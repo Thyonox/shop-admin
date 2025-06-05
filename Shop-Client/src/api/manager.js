5e6179140794a48ed12a26750b364d4141b415b4
thyonix <dongf7889@gmail.com>
2025-06-05 23:29:02 +0800

    [Shop-Client] - Manager API

---
+++ Shop-Client/src/api/manager.js
@@ -0,0 +1,24 @@
+import instance from "../utils/request";
+
+// 登录
+export function login(username,password) {
+    return instance.post('admin/login', {
+        username,
+        password
+    })
+}
+
+// 获取用户信息
+export function getInfo(){
+    return instance.post('/admin/getinfo')
+}
+
+// 退出登录
+export function logout(){
+    return instance.post('/admin/logout')
+}
+
+// 修改密码
+export function changePassword(data){
+    return instance.post('/admin/updatepassword', data)
+}
