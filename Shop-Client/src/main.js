13344b32dcea9dade9950d5ac432fe1e7e3ec57a
thyonix <dongf7889@gmail.com>
2025-06-06 05:40:01 +0800

    [Shop-Client] - Import ElementPlus Icon

--- Shop-Client/src/main.js
+++ Shop-Client/src/main.js
@@ -4,6 +4,7 @@ import './assets/style/globalStyle.css'
 // Import ElementPlus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 // Import RemixIcon
 import 'remixicon/fonts/remixicon.css'
 // Import VueRouter
@@ -14,6 +15,11 @@ import './permission.js';
 import App from './App.vue'
 
 const app = createApp(App);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
 app.use(ElementPlus);
 app.use(router);
 app.use(store);
